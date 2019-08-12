416d4006ff9b53bf7ceeef595c522ef44221923d
Pranav <pranav.kumar@silicus.com>
2019-08-12 09:25:16 +0530

    advance filter done

--- backend/v1/apps/lead/filters.py
+++ backend/v1/apps/lead/filters.py
@@ -9,19 +9,16 @@ class LeadFilter(django_filters.FilterSet):
     def q_filter(self, queryset, name, val):
         try:
             val=val.replace("'", '"')
-            print(val)
             filter_q = json.loads(val)
             if 'created_on__date__range' in filter_q.keys():
                 filter_q['created_on__date__range'] = filter_q['created_on__date__range'].split(",")
         except Exception as e:
             print("Exception", e)
             return queryset
-        print("filtes", filter_q)
         return queryset.annotate(
                 full_name=Concat(
                     F('business_detail__first_name'), Value(" "), F('business_detail__middle_name'), Value(" "), F('business_detail__last_name'))
             ).filter(**filter_q)
-
     class Meta:
         model = Lead
         fields = ('q',)
