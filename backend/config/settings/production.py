83fa5714d1d79f09583fe2571df1324ca05cd3b8
pranav <pranav.kumar@silicus.com>
2020-03-09 07:24:09 +0530

    db removed from git

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -9,17 +9,6 @@ INTERNAL_IPS = [
     '127.0.0.1',
 ]
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'defaultdb',
-        'USER': 'doadmin',
-        'PASSWORD': 'x26fsuuis53jgb4o',
-        'HOST': 'db-postgresql-blr1-41581-do-user-5955133-0.db.ondigitalocean.com',
-        'PORT': '25060',
-        'OPTIONS': {'sslmode': 'require'},
-    }
-}
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
